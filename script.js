37d996c0bc3280d6201e08c88b594cab7261c248
PriyaNayak28 <nayakpriya612@gmail.com>
2024-02-25 00:25:35 +0530

    completed task req & res 1st|7th

--- script.js
+++ script.js
@@ -8,9 +8,24 @@ fs.writeFileSync('script.txt', 'dummy txt');
 const http = require('http');
 
 const server = http.createServer((req, res) => {
+    console.log(req.url, req.method, req.headers);
+    res.setHeader('content-Type', 'text/html')
+    res.write(`<html>
+<head><title>Document</title></head>
+<body><h1>hello world!</h1></body>
+</html>`)
+    if (req.url === '/home') {
+        res.write('welcome home');
+    }
+    else if (req.url === '/about') {
+        res.write('welcome to about us page');
+    }
+    else if (req.url === '/node') {
+        res.write('Welcome to my Node Js project');
+    }
     res.end("<h1>priya nayak</h1>");
 });
 
-server.listen(3000, () => {
+server.listen(4000, () => {
     console.log("server started");
 })
