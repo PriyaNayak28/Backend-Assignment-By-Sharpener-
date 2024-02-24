2f3313c8af858367c3d8dd85663e63d828fabb29
PriyaNayak28 <nayakpriya612@gmail.com>
2024-02-24 19:51:38 +0530

    completed task 1st.06 no learned about createServer

--- script.js
+++ script.js
@@ -2,3 +2,15 @@ console.log("hello world");
 
 const fs = require('fs');
 fs.writeFileSync('script.txt', 'dummy txt');
+
+// all about create server
+
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    res.end("<h1>priya nayak</h1>");
+});
+
+server.listen(3000, () => {
+    console.log("server started");
+})
