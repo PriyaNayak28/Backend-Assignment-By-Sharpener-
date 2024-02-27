cd27852579d6b7b3707e5fd370c2f3836382a1ba
PriyaNayak28 <nayakpriya612@gmail.com>
2024-02-27 19:50:11 +0530

    completed 1st|8th task Writing and reading from a file

--- script.js
+++ script.js
@@ -1,31 +1,77 @@
-console.log("hello world");
+// creating a small chat app
 
 const fs = require('fs');
-fs.writeFileSync('script.txt', 'dummy txt');
-
-// all about create server
-
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.method, req.headers);
-    res.setHeader('content-Type', 'text/html')
-    res.write(`<html>
-<head><title>Document</title></head>
-<body><h1>hello world!</h1></body>
-</html>`)
-    if (req.url === '/home') {
-        res.write('welcome home');
+    const url = req.url;
+    const method = req.method;
+
+    if (url === '/') {
+        res.write(`<html>
+           <head><title>Document</title></head>
+           <body>
+           <form action ="/message" method = "POST">
+           <input type ='text' name = "message">
+           <button type="submit">submit</button>
+           </form>
+           </body>
+           </html>`);
+        return res.end();
     }
-    else if (req.url === '/about') {
-        res.write('welcome to about us page');
+
+    if (url === '/message' && method === 'POST') {
+        const body = [];
+        req.on("data", (chunk) => {
+            console.log(chunk)
+            body.push(chunk);
+        });
+
+        req.on('end', () => {
+            const parseBody = Buffer.concat(body).toString();
+            const message = parseBody.split('=')[1];
+            fs.writeFile('message.txt', message, (err) => {
+                if (err) {
+                    console.log(err);
+                    return res.end('Error writing to file');
+                }
+
+                fs.readFile('message.txt', 'utf8', (err, data) => {
+                    if (err) {
+                        console.log(err);
+                        return res.end('Error reading file');
+                    }
+                    else {
+                        res.setHeader('location', '/');
+                        res.write(`<html>
+                        <head><title>Document</title></head>
+                        <body>
+                        <form action ="/message" method = "POST">
+                        <h1>${data}</h1>
+                        <input type ='text' name = "message">
+                        <button type="submit">submit</button>
+                        </form>
+                        </body>
+                        </html>`);
+                        console.log(data);
+                        return res.end();
+                    }
+                });
+            });
+        })
     }
-    else if (req.url === '/node') {
-        res.write('Welcome to my Node Js project');
-    }
-    res.end("<h1>priya nayak</h1>");
 });
 
-server.listen(4000, () => {
+// server started
+
+server.listen(5500, () => {
     console.log("server started");
 })
+
+
+
+
+
+
+
+
