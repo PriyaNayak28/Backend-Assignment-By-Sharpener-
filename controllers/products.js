d00b5cd5c8306b3a5c46566553d26a57eaed66d2
PriyaNayak28 <nayakpriya612@gmail.com>
2024-03-06 18:37:04 +0530

    task completed 02.06 What the hell are Models?

--- controllers/products.js
+++ controllers/products.js
@@ -1,17 +1,23 @@
 const path = require('path');
 const rootDir = require('../utils/path');
+const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
     res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 }
 
 exports.postAddProduct = (req, res, next) => {
-    console.log(req.body);
+    const product = new Product(req.body.title);
+    console.log(product);
+    product.save();
+    console.log(product.save());
     res.redirect('/');
 }
 
 exports.getProducts = (req, res, next) => {
-    res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+    const products = Product.fetchAll((products) => {
+        res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+    });
 };
 
 exports.getContact = (req, res, next) => {
@@ -26,3 +32,7 @@ exports.postContact = (req, res, next) => {
 exports.pageNotFound = (req, res, next) => {
     res.sendFile(path.join(rootDir, 'views', '404.html'));
 };
+
+
+
+
